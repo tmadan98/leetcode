28ee48e2c07b88bb6786f50aa836d94caaf350b4
tarik-madan <tarik-madan@Madan>
2026-07-28 01:18:51 +0200

    Secibd Commit

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -5,5 +5,6 @@ public class TwoSum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Commit");
+		System.out.println("Second Commit");
 	}
 }
