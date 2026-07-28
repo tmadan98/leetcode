ff399bda91d5773af97e1a28911fec88ca94d64f
tarik-madan <tarik-madan@Madan>
2026-07-28 02:03:41 +0200

    Solved 1. Two Sum

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -1,11 +1,18 @@
 package arrays;
 
 public class TwoSum {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("First Commit");
-		System.out.println("Second Commit");
-		System.out.println("Third Commit");
-	}
+	
+    public int[] twoSum(int[] nums, int target) {
+    	
+    	for(int i = 0; i < nums.length; i++) {
+    		for(int j = 0; j < nums.length; j++) {
+    			if(i != j) {
+    				if(nums[i] + nums[j] == target) {
+    					return new int[]{i, j};
+    				}
+    			}
+    		}
+    	}
+    	return new int[]{};
+    }
 }
